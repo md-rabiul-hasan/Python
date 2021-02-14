3c686f0c9c67795f4df3c8bdb3505b5748a1c2e1
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-02-14 07:57:53 +0600

    number

---
+++ number.py
@@ -0,0 +1,10 @@
+print(2 + 2)
+# 4
+print( 4 / 2) # division always return float number
+# 2.0
+print(17 // 3)  # floor division discards the fractional part
+# 5
+
+print(5 ** 2)  # 5 squared = 25
+
+print(5 ** 3) # 5 to the power of 3 = 125

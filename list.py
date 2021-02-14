0f60dceae715ce34b40c44dfe4f8f0a8a0158ced
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-02-14 08:04:22 +0600

    list

---
+++ list.py
@@ -0,0 +1,6 @@
+squares = [1, 4, 9, 16, 25]
+print(squares)
+print(squares[0])  # indexing returns the item = 1
+print(squares[:3]) # slicing returns a new list [1, 4, 9]
+print(squares[:]) # [1, 4, 9, 16, 25]
+print(len(squares)) # 5 count list
